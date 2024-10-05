a9a51c49df24b2784d63ce594f010338f78745b6
CareerByteCode <careerbytec@gmail.com>
2024-10-05 08:01:31 +0200

    update

--- aws/class1/create-ec2-instance/webserver.tf
+++ aws/class1/create-ec2-instance/webserver.tf
@@ -1,13 +1,13 @@
 resource "aws_instance" "My-Webserver" {
 
- ami = "ami-0a313d6098716f372"
- instance_type = "t2.micro"
- vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
- tags = {
-	 Name = "My-Webserver"
- }
- key_name = "terraform"
- user_data = <<EOF
+  ami                    = "ami-0a313d6098716f372"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.webserver_sg.id}"]
+  tags = {
+    Name = "My-Webserver"
+  }
+  key_name  = "careerbytecode"
+  user_data = <<EOF
 #!/bin/bash -xe
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 /usr/bin/apt-get update
