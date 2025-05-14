38098843e0da8558666699c437cc7a7cffa9df83
vibincholayil <vibinnath.c@gmail.com>
2025-05-14 14:07:29 +0100

    add

--- aws/class1/create-ec2-instance/securitygroups.tf
+++ aws/class1/create-ec2-instance/securitygroups.tf
@@ -1,26 +1,28 @@
 resource "aws_security_group" "webserver_sg" {
-  vpc_id = vpc-024442e13996eb6e4
+  vpc_id = "vpc-024442e13996eb6e4"
 
   ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0"]
-    }
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = [ "0.0.0.0/0"]
-    }
-egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }    
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "webserver-sg"
+  }
 }
-
-
-    
